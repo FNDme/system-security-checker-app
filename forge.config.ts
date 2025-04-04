4c297117e3e828da84a3624eb3b9bb63327a1545
Gabriel Luis Freitas <gabiluisfreitas@gmail.com>
2025-04-04 18:32:26 +0200

    fix: Bump appVersion to 1.0.1 in Forge configuration for version tracking

--- forge.config.ts
+++ forge.config.ts
@@ -15,7 +15,7 @@ const config: ForgeConfig = {
     appCopyright: "Expero Inc.",
     asar: true,
     icon: "./build/assets/icons/icon",
-    appVersion: "1.0.0",
+    appVersion: "1.0.1",
     extraResource: ["./build/assets"],
   },
   rebuildConfig: {},
